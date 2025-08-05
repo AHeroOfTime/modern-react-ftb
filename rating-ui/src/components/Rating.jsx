dc0484bd5a1af9552dea51faa1c8b81057f1fe65
Aaron Rodriguez <aaronerdev@gmail.com>
2025-08-04 21:10:17 -0500

    styling in react

--- rating-ui/src/components/Rating.jsx
+++ rating-ui/src/components/Rating.jsx
@@ -1,5 +1,18 @@
 const Rating = () => {
-  return <div>Rate Your Experience</div>;
+  return (
+    // <div style={styles.container}></div>
+    <div className='rating-container'>
+      <h2>Rate Your Experience</h2>
+    </div>
+  );
 };
+
+// const styles = {
+//   container: {
+//     textAlign: 'center',
+//     fontFamily: 'Arial',
+//     padding: '20px',
+//   },
+// };
 
 export default Rating;
