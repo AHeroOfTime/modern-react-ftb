efd811784cfecd6ac2611bab3553ae61549b8bdb
Aaron Rodriguez <aaronerdev@gmail.com>
2025-08-04 21:19:10 -0500

    lists

--- rating-ui/src/components/Rating.jsx
+++ rating-ui/src/components/Rating.jsx
@@ -1,8 +1,17 @@
 const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
   return (
     // <div style={styles.container}></div>
     <div className='rating-container'>
       <h2>Rate Your Experience</h2>
+      <div className='stars'>
+        {stars.map((star) => (
+          <span key={star} className='star'>
+            {'\u2605'}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
