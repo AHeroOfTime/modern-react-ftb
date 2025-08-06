85fbb382f9a816ff0a9c2042a8a5c7f9c4eaab32
Aaron Rodriguez <aaronerdev@gmail.com>
2025-08-06 18:22:48 -0500

    component props

--- rating-ui/src/components/Rating.jsx
+++ rating-ui/src/components/Rating.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 
-const Rating = () => {
+const Rating = ({
+  heading = 'Rate Your Experience',
+  color = 'gold',
+  feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent'],
+}) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-  const feedbackMessages = ['Terrible', 'Poor', 'Fair', 'Good', 'Excellent'];
 
   return (
     // <div style={styles.container}></div>
     <div className='rating-container'>
-      <h2>Rate Your Experience</h2>
+      <h2>{heading}</h2>
       <div className='stars'>
         {stars.map((star) => (
           <span
@@ -18,7 +21,10 @@ const Rating = () => {
             onMouseLeave={() => setHover(0)}
             onClick={() => setRating(star)}
             key={star}
-            className={`star ${star <= (hover || rating) ? 'active' : ''}`}
+            className='star'
+            style={{
+              color: star <= (hover || rating) ? color : '#ccc',
+            }}
           >
             {'\u2605'}
           </span>
