658400a5477e8b104cffffbb41ba5bf596c804c1
Aaron Rodriguez <aaronerdev@gmail.com>
2025-08-20 17:35:36 -0500

    display coin data

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import CoinCard from './components/CoinCard';
+
 const API_URL =
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
@@ -12,7 +14,7 @@ const App = () => {
       try {
         const res = await fetch(API_URL);
         if (!res.ok) throw new Error('Failed to fetch data');
-        const data = res.json();
+        const data = await res.json();
         console.log(data);
         setCoins(data);
       } catch (err) {
@@ -43,6 +45,16 @@ const App = () => {
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className='error'>{error}</div>}
+
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map((coin) => (
+            <CoinCard coin={coin} key={coin.id} />
+          ))}
+        </main>
+      )}
     </div>
   );
 };
